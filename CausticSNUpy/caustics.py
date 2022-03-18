085321ce8b78818bf5742bc90bf622b36e4c729e
woodykang <woodykang@snu.ac.kr>
2022-03-18 22:46:18 +0900

    Revert "[update]"
    This reverts commit bc254f3af06f415d84f26b4ad615bb3e2aba14ce.

--- CausticSNUpy/caustics.py
+++ CausticSNUpy/caustics.py
@@ -16,11 +16,16 @@ import astropy.coordinates
 import astropy.units
 import astropy.cosmology
 import skimage.measure
+<<<<<<< HEAD:CausticSNUpy/caustics.py
 from .hierarchical_clustering import hier_clustering
 
 <<<<<<< HEAD:CausticSNUpy/caustics.py
 class Caustics:
 =======
+=======
+from hierarchical_clustering import hier_clustering
+
+>>>>>>> parent of bc254f3 ([update]):CausticSNUpy.py
 class caustics:
 >>>>>>> parent of bc254f3 ([update]):CausticSNUpy.py
 
